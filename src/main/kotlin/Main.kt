e45444d59d823a532917cd44b9df03bfc32df9c5
joohwan <joohwan9607@mobility42.io>
2023-06-16 19:44:12 +0900

    Atomic Kotlin 1. 프로그래밍 기초 : 1 ~ 4

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    println("Hello World")
+    var whole = 11
+    var fractional = 1.4
+    var words = "Twas Brillig"
+    println(whole)
+    println(fractional)
+    println(words)
+    var sum = 1
+    sum += 2
+    sum += 3
+    println(sum)
+}
