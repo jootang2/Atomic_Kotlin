7edbe43c0dc38345c94403a12bc434566ee4be59
joohwan <joohwan9607@mobility42.io>
2023-06-28 19:01:24 +0900

    spiral 만들기

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,84 +1,122 @@
+//import java.util.*
+//
+///**
+// * spiral 만들기
+// */
+//
+//var n = 0
+//var m = 0
+//var spiralList = mutableListOf<MutableList<Int>>()
+//var number = 0
+//var flag = true
+//
+//fun up(x: Int, y: Int) {
+//    //map을 벗어낫 경우
+//    if (x < 0 || x > n - 1 || y < 0 || y > m - 1) {
+//        right(x, y)
+//    }
+//    // 사방이 모두 값이 할당되어있는 경우 - 멈춰야 함
+//    else if(spiralList[x + 1][y] != 0 && spiralList[x - 1][y] != 0 && spiralList[x][y+1] != 0 && spiralList[x + 1][y-1] != 0){
+//        flag = false
+//    } else {
+//        // spiralArray에 number 값 할당
+//        spiralList[x][y] = number
+//        // number 1 증가
+//        number++
+//        up(x - 1, y)
+//    }
+//}
+//
+//fun left(x: Int, y: Int) {
+//    if (x < 0 || x > n - 1 || y < 0 || y > m - 1) {
+//        up(x, y)
+//    }
+//    // 사방이 모두 값이 할당되어있는 경우 - 멈춰야 함
+//    else if(spiralList[x + 1][y] != 0 && spiralList[x - 1][y] != 0 && spiralList[x][y+1] != 0 && spiralList[x + 1][y-1] != 0){
+//        flag = false
+//    } else {
+//        // spiralArray에 number 값 할당
+//        spiralList[x][y] = number
+//        // number 1 증가
+//        number++
+//        left(x, y - 1)
+//    }
+//}
+//
+//fun down(x: Int, y: Int) {
+//    if (x < 0 || x > n - 1 || y < 0 || y > m - 1) {
+//        left(x, y)
+//    }
+//    // 사방이 모두 값이 할당되어있는 경우 - 멈춰야 함
+//    else if(spiralList[x + 1][y] != 0 && spiralList[x - 1][y] != 0 && spiralList[x][y+1] != 0 && spiralList[x + 1][y-1] != 0){
+//        flag = false
+//    } else {
+//        // spiralArray에 number 값 할당
+//        spiralList[x][y] = number
+//        // number 1 증가
+//        number++
+//        down(x + 1, y)
+//    }
+//}
+//
+//// 오른쪽으로 이동 함수
+//fun right(x: Int, y: Int) {
+//    if (x < 0 || x > n - 1 || y < 0 || y > m - 1) {
+//        left(x, y)
+//    }
+//    else if(spiralList[x + 1][y] != 0 && spiralList[x - 1][y] != 0 && spiralList[x][y+1] != 0 && spiralList[x + 1][y-1] != 0){
+//        flag = false
+//    }
+//    else {
+//        // spiralArray에 number 값 할당
+//        spiralList[x][y] = number
+//        // number 1 증가
+//        number++
+//        right(x + 1, y)
+//    }
+//}
+//
+//fun main() {
+//    val sc = Scanner(System.`in`)
+//    // m : 행의 수
+//    m = sc.nextInt()
+//    // n : 열의 수
+//    n = sc.nextInt()
+//    while(flag){
+//        right(0, 0)
+//    }
+//    // spiralArray 출력
+//    for (row in spiralList) {
+//        println(row.joinToString(" "))
+//    }
+//}
+
 fun main() {
-    /**
-     * 12. 루프와 범위
-     */
-    fun showRange(r: IntProgression) {
-        for (i in r) {
-            print("$i ")
+    var test = mutableListOf<MutableList<Int>>()
+    val m = 5
+    val n = 5
+    var list1 = mutableListOf<Int>()
+    for(i in 0 until m){
+        list1.add(0)
+    }
+
+    for(i in 0 until m){
+        test.add(list1)
+    }
+
+    for (row in test) {
+        println(row.joinToString { " " })
+    }
+
+    for (i in list1) {
+        println("$i")
+    }
+
+    println(test[0][1])
+    for (row in test) {
+        for (element in row) {
+            print("$element ")
         }
-        println(" // $r")
         println()
     }
-
-    showRange(1..5)
-    showRange(1 until 5)
-    showRange(5 downTo 1)
-    showRange(0..5 step 2)
-    showRange(0 until 5 step 2)
-    showRange(10 downTo 2 step 2)
-
-    for (i in 'a'..'z') {
-        print("$i")
-    }
-
-    println()
-
-    val a = "abcd"
-    for (i in 0..a.lastIndex) {
-        print(a[i] + 1)
-    }
-
-    println()
-
-    val ch: Char = 'a'
-    println(ch + 25)
-    println(ch + 25 < 'z')
-    println('y' < 'z')
-
-    println()
-
-    fun hasChar(s: String, c: Char): Boolean {
-        for (i in s) {
-            if (i == c) return true
-        }
-        return false
-    }
-
-    println(hasChar("abc", 'c'))
-    println(hasChar("abc", 'z'))
-
-    /**
-     * 13. in 키워드
-     */
-    println()
-    println("13. in 키워드")
-
-    val percent = 35
-    println(percent in 1..20)
-    println(percent in 1..36)
-
-    // for 루프 제어식에 있는 in만 이터레이션을 뜻한다.
-    // 나머지 in은 모두 원소인지 여부를 검사하는 in이다.
-
-    val b = 1..3
-    for (i in b) {
-        print("$i")
-    }
-    println()
-    println(2 in b)
-    println(4 in b)
-
-
-    println()
-    println()
-    //25. 가변 인자 목록
-    val aa = listOf(1)
-    println(aa)
-
-    fun v(s: String, vararg d: Double) {}
-    v("Abc", 1.0, 2.0)
-    v("Abc", 1.0, 2.0, 3.0)
-    v("Abc", 1.0, 2.0, 4.0, 5.0, 6.90)
-
-
 }
